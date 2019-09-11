39c79dc03838c6835028a712b1d3d124f2490797
Atsushi Ando <ando@next-season.net>
2019-09-11 16:06:58 +0900

    First commit

---
+++ hello-shifter-github-hosting-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Hello Shifter GitHub hosting plugin
+Plugin URI: https://github.com/AtsushiA/hello-shifter-github-hosting-plugin
+Description: for Shifter GitHub hosting plugin actions DEMO
+Author: Atsushi Ando
+Version: {release version}
+Author URI: https://next-season.net/
+*/
