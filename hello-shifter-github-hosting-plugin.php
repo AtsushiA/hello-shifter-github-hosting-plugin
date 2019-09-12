60fca44fabe521d972777e599eeb9d4f9feae74b
Atsushi Ando <ando@next-season.net>
2019-09-12 20:17:37 +0900

    replace maru

--- hello-shifter-github-hosting-plugin.php
+++ hello-shifter-github-hosting-plugin.php
@@ -7,3 +7,9 @@ Author: Atsushi Ando
 Version: {release version}
 Author URI: https://next-season.net/
 */
+
+add_filter('the_content', 'moji_changer_content_filter');
+
+function moji_changer_content_filter($content) {
+    return str_replace('。', '<span style="color:red;font-size:200%;">。</span>', $content);
+}
